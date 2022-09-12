c755c1d8d7bea7721beec360684d9d644759aac2
goocarry <goocarry@gmail.com>
2022-09-12 19:34:18 +0900

    separate handlers, server tuning

---
+++ handlers/hello.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Hello ...
+type Hello struct {
+	logger *log.Logger
+}
+
+// NewHello ...
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{
+		logger: l,
+	}
+}
+
+// ServeHTTP ...
+func (h Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.logger.Println("home route called")
+
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "Hello %s", d)
+
+}
--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/goocarry/mstemplate/handlers"
 )
 
 func main() {
 	log.Println("Hello microservices")
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		d, err := ioutil.ReadAll(r.Body)
+	logger := log.New(os.Stdout, "api", log.LstdFlags)
+	helloHandler := handlers.NewHello(logger)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", helloHandler)
+
+	s := http.Server{
+		Addr:         ":9990",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		ReadTimeout:  1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-
-			// rw.WriteHeader(http.StatusBadRequest)
-			// rw.Write([]byte("Ooops"))
-
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
+			logger.Fatalf("server error: %s", err)
 		}
+	}()
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.ListenAndServe(":9990", nil)
+	sig := <-sigChan
+	logger.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	s.Shutdown(tc)
 }
